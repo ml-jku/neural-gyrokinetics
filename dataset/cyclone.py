flux computation fix

dataset/cyclone.py
@@ -34,7 +34,7 @@ class CycloneSample:
 class CycloneDataset(Dataset):
     def __init__(
         self,
-        path: str = "/system/user/publicdata/gyrokinetics_preprocessed",
+        path: str = "/restricteddata/ukaea/gyrokinetics/preprocessed",
         split: str = "train",
         active_keys: Optional[List[str]] = None,
         trajectories: Optional[List[str]] = None,
@@ -329,10 +329,21 @@ class CycloneDataset(Dataset):
 
         return sample
 
-    def get_timesteps_only(self, file_idx: torch.Tensor, timestep_idx: torch.Tensor):
+    def get_timesteps(
+        self, file_idx: torch.Tensor, timestep_idx: Optional[torch.Tensor] = None
+    ):
         # file_idx: (B,)
-        # timestep_idx: (B, N)
         file_idx = file_idx.cpu().long()
+        # all timesteps in file
+        if timestep_idx is None:
+            timestep_idx = torch.stack(
+                [
+                    torch.arange(self.num_ts(file_idx[i]) - 1)
+                    for i in range(file_idx.shape[0])
+                ],
+                dim=0,
+            )
+        # timestep_idx: (B, N)
         timestep_idx = timestep_idx.cpu().long()
 
         B = file_idx.shape[0]

eval/rollout.py
@@ -63,7 +63,7 @@ def get_rollout(
             ]
             for ts_idx_start in ts_index_0.tolist()
         ]
-        tsteps = dataset.get_timesteps_only(file_idx, torch.tensor(ts_idxs))
+        tsteps = dataset.get_timesteps(file_idx, torch.tensor(ts_idxs))
         use_bf16 = use_amp and torch.cuda.is_bf16_supported()
         with torch.no_grad():
             # move bundles forward, rollout in blocks

train/losses.py
@@ -84,7 +84,7 @@ def get_pushforward_trick(
             ]
             for ts_idx_start in ts_idx.tolist()
         ]
-        tsteps = dataset.get_timesteps_only(file_idx, torch.tensor(ts_idxs))
+        tsteps = dataset.get_timesteps(file_idx, torch.tensor(ts_idxs))
 
         # get unrolled target in a non-blocking way
         def fetch_target(dataset, file_idx, ts_unrolled):

utils.py
@@ -98,10 +98,16 @@ def save_model_and_config(
     with open(os.path.join(cfg.ckpt_path, "config.yaml"), "w") as f:
         OmegaConf.save(config=cfg, f=f.name)
 
+    state_dict = model.state_dict()
+
+    if hasattr(model, "module"):
+        # using DDP wrapper
+        state_dict = {k.replace("module.", ""): v for k, v in state_dict.items()}
+
     torch.save(
         {
             "epoch": epoch,
-            "model_state_dict": model.state_dict(),
+            "model_state_dict": state_dict,
             "optimizer_state_dict": optimizer.state_dict(),
             "loss": val_loss,
         },
@@ -113,7 +119,7 @@ def save_model_and_config(
         torch.save(
             {
                 "epoch": epoch,
-                "model_state_dict": model.state_dict(),
+                "model_state_dict": state_dict,
                 "optimizer_state_dict": optimizer.state_dict(),
                 "loss": val_loss,
             },