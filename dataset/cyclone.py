fixed dataset-wide normalization

dataset/cyclone.py
@@ -12,6 +12,7 @@ from torch.utils.data import Dataset
 
 import random
 
+from dataset.utils import RunningMeanStd
 from utils import expand_as
 
 
@@ -86,7 +87,7 @@ class CycloneDataset(Dataset):
         else:
             self.active_keys = np.arange(4)
         assert normalization in ["zscore", "minmax", "none", None]
-        assert normalization_scope in ["sample", "dataset", "per_mode"]
+        assert normalization_scope in ["sample", "dataset", "per_mode", "trajectory"]
         self.normalization = normalization if normalization != "none" else None
         self.normalization_scope = normalization_scope
         self.cond_filters = cond_filters
@@ -165,6 +166,7 @@ class CycloneDataset(Dataset):
         self.steps_per_file = {}
         self.dataset_stats = defaultdict(dict)
         per_file_t_indexes = []
+        stats = None
         for file_idx, file_path in enumerate(self.files):
             # check for holdout samples
             filename = os.path.split(file_path)[-1]
@@ -202,6 +204,19 @@ class CycloneDataset(Dataset):
                 self.dataset_stats[file_idx]["std"] = f["metadata/k_std"][:]
                 self.dataset_stats[file_idx]["min"] = f["metadata/k_min"][:]
                 self.dataset_stats[file_idx]["max"] = f["metadata/k_max"][:]
+                if stats is None and normalization_scope == "dataset":
+                    stats = RunningMeanStd(shape=self.dataset_stats[file_idx]["mean"].shape)
+                if stats is not None:
+                    stats.update(self.dataset_stats[file_idx]["mean"],
+                                 self.dataset_stats[file_idx]["std"]**2,
+                                 self.dataset_stats[file_idx]["min"],
+                                 self.dataset_stats[file_idx]["max"])
+
+        if normalization_scope == "dataset":
+            self.dataset_stats["full"]["mean"] = stats.mean
+            self.dataset_stats["full"]["std"] = stats.var ** (1/2)
+            self.dataset_stats["full"]["min"] = stats.min
+            self.dataset_stats["full"]["max"] = stats.max
 
         self.cumulative_samples = np.cumsum([0] + self.file_num_samples)
         self.length = self.cumulative_samples[-1]
@@ -280,7 +295,7 @@ class CycloneDataset(Dataset):
         with h5py.File(self.files[0], "r") as f:
             self.resolution = f["metadata/resolution"][:]
 
-    def __getitem__(self, index: int) -> CycloneSample:
+    def __getitem__(self, index: int, get_normalized: bool = True) -> CycloneSample:
         """
         Args:
             index (int): Flat index with dataset ordering.
@@ -310,7 +325,7 @@ class CycloneDataset(Dataset):
         poten, flux = sample["gt_poten"], sample["gt_flux"]
         # conditioning fields
         timestep, itg = sample["timestep"], sample["itg"]
-        if self.normalization is not None:
+        if self.normalization is not None and get_normalized:
             x = self._normalize(x, file_index)
             gt = self._normalize(gt, file_index)
 
@@ -379,6 +394,7 @@ class CycloneDataset(Dataset):
         shift, scale = 0.0, 1.0
         if self.apply_log:
             x = np.log(1 + np.abs(np.min(x)) + x)
+
         if self.normalization_scope == "sample":
             if self.normalization == "zscore":
                 shift = x.mean((1, 2, 3, 4, 5), keepdims=True)
@@ -388,17 +404,46 @@ class CycloneDataset(Dataset):
                 x_max = x.max((1, 2, 3, 4, 5), keepdims=True)
                 shift = x_min
                 scale = x_max - x_min
-        if self.normalization_scope == "dataset":
+        else:
+            if self.normalization_scope == "dataset":
+                key = "full"
+            elif self.normalization_scope == "trajectory":
+                key = file_index
+            else:
+                raise NotImplementedError
+
             if self.normalization == "zscore":
-                shift = expand_as(self.dataset_stats[file_index]["mean"], x)
-                scale = expand_as(self.dataset_stats[file_index]["std"], x)
+                shift = expand_as(self.dataset_stats[key]["mean"], x)
+                scale = expand_as(self.dataset_stats[key]["std"], x)
             if self.normalization == "minmax":
-                x_min = expand_as(self.dataset_stats[file_index]["min"], x)
-                x_max = expand_as(self.dataset_stats[file_index]["max"], x)
+                x_min = expand_as(self.dataset_stats[key]["min"], x)
+                x_max = expand_as(self.dataset_stats[key]["max"], x)
                 shift = x_min
                 scale = x_max - x_min
 
         return (x - shift) / scale
+
+    def denormalize(self, x, file_index):
+        shift, scale = 0.0, 1.0
+        if self.normalization_scope == "sample":
+            raise NotImplementedError
+        else:
+            if self.normalization_scope == "dataset":
+                key = "full"
+            if self.normalization_scope == "trajectory":
+                key = file_index
+
+            if self.normalization == "zscore":
+                shift = expand_as(self.dataset_stats[key]["mean"], x)
+                scale = expand_as(self.dataset_stats[key]["std"], x)
+            if self.normalization == "minmax":
+                x_min = expand_as(self.dataset_stats[key]["min"], x)
+                x_max = expand_as(self.dataset_stats[key]["max"], x)
+                shift = x_min
+                scale = x_max - x_min
+        scale = torch.from_numpy(scale).to(x.device)
+        shift = torch.from_numpy(shift).to(x.device)
+        return x * scale + shift
 
     def _conditioning_filter(self, fname: str) -> bool:
         with h5py.File(fname, "r") as f:

dataset/preprocess.py
@@ -4,62 +4,9 @@ import h5py
 import re
 from tqdm import tqdm
 
+from .utils import RunningMeanStd
+
 ROOT = "/restricteddata/ukaea/gyrokinetics"
-
-class RunningMeanStd:
-    def __init__(self, epsilon: float = 1e-4, shape: tuple[int, ...] = ()):
-        """
-        Calculates the running mean and std of a data stream
-        https://en.wikipedia.org/wiki/Algorithms_for_calculating_variance#Parallel_algorithm
-
-        :param epsilon: helps with arithmetic issues
-        :param shape: the shape of the data stream's output
-        """
-        self.mean = np.zeros(shape, np.float32)
-        self.min = np.zeros(shape, np.float32)
-        self.max = np.zeros(shape, np.float32)
-        self.var = np.ones(shape, np.float32)
-        self.count = epsilon
-
-    def copy(self) -> "RunningMeanStd":
-        """
-        :return: Return a copy of the current object.
-        """
-        new_object = RunningMeanStd(shape=self.mean.shape)
-        new_object.mean = self.mean.copy()
-        new_object.var = self.var.copy()
-        new_object.count = float(self.count)
-        return new_object
-
-    def combine(self, other: "RunningMeanStd") -> None:
-        """
-        Combine stats from another ``RunningMeanStd`` object.
-
-        :param other: The other object to combine with.
-        """
-        self.update_from_moments(other.mean, other.var, other.min, other.max, other.count)
-
-    def update(self, mean, var, min, max) -> None:
-        self.update_from_moments(mean, var, min, max)
-
-    def update_from_moments(self, batch_mean: np.ndarray, batch_var: np.ndarray, batch_min: np.ndarray, batch_max: np.ndarray,
-                            batch_count: float = 1.) -> None:
-        delta = batch_mean - self.mean
-        tot_count = self.count + batch_count
-
-        new_mean = self.mean + delta * batch_count / tot_count
-        m_a = self.var * self.count
-        m_b = batch_var * batch_count
-        m_2 = m_a + m_b + np.square(delta) * self.count * batch_count / (self.count + batch_count)
-        new_var = m_2 / (self.count + batch_count)
-
-        new_count = batch_count + self.count
-
-        self.min = np.minimum(self.min, batch_min)
-        self.max = np.maximum(self.max, batch_max)
-        self.mean = new_mean
-        self.var = new_var
-        self.count = new_count
 
 def K_files(directory):
     files = os.listdir(directory)
@@ -198,7 +145,7 @@ def get_stats(filenames, spatial_ifft=False, separate_zf=False, per_mode_norm=Fa
 def preprocess(filename, spatial_ifft=False, separate_zf=False, stats=None, per_mode_norm=False):
     assert not (separate_zf and not spatial_ifft), "Need to perform IFFT to maintain shapes for separate_zf"
     dir_in = f"{ROOT}/raw/{filename}"
-    dir_out = f"/local00/bioinf/gyrokinetics/preprocessed"
+    dir_out = f"{ROOT}/preprocessed"
     if not os.path.exists(dir_out):
         os.makedirs(dir_out)
 
@@ -339,15 +286,13 @@ ifft_tag = "_ifft" if IFFT else ""
 zf_tag = "_separate_zf" if separate_zf else ""
 per_mode_tag = "_per_mode" if per_mode_norm else ""
 datasets = [
-    "cyclone4_2_2",
-    "cyclone5_2",
-    "cyclone6_2",
-    "cyclone7_2",
-    "cyclone8_2",
-    "cyclone9_2",
-    "cyclone10_2",
-    "cyclone11_2",
-    "cyclone12_2",
+    "cyclone19_2",
+    "cyclone20_2",
+    "cyclone21_2",
+    "cyclone22_2",
+    "cyclone22_2_diffInit",
+    "cyclone22_2_diffInit2",
+    "cyclone22_2_diffInit3",
 ]
 
 stats = get_stats(datasets, IFFT, separate_zf, per_mode_norm) if per_mode_norm else None