minor fixes for normalization, plotting, init

dataset/cyclone.py
@@ -62,6 +62,7 @@ class CycloneDataset(Dataset):
         partial_holdouts: Optional[dict] = None,
         normalization: Optional[str] = "zscore",
         normalization_scope: str = "sample",
+        normalization_stats: Optional[Dict[str, float]] = None,
         spatial_ifft: bool = True,
         cond_filters: Optional[Dict[str, Sequence]] = None,
         dtype: Type = torch.float32,
@@ -88,6 +89,8 @@ class CycloneDataset(Dataset):
         assert normalization in ["zscore", "minmax", "none", None]
         assert normalization_scope in ["sample", "dataset", "per_mode", "trajectory"]
         self.normalization = normalization if normalization != "none" else None
+        if normalization_stats is not None:
+            self.dataset_stats = normalization_stats
         self.normalization_scope = normalization_scope
         self.cond_filters = cond_filters
         self.subsample = subsample
@@ -162,7 +165,8 @@ class CycloneDataset(Dataset):
         self.file_num_samples = []
         self.file_num_timesteps = []
         self.steps_per_file = {}
-        self.dataset_stats = defaultdict(dict)
+        if normalization_stats is None:
+            self.dataset_stats = defaultdict(dict)
         per_file_t_indexes = []
         stats = None
         for file_idx, file_path in enumerate(self.files):
@@ -200,21 +204,22 @@ class CycloneDataset(Dataset):
                 self.file_num_samples.append(n_samples)
                 self.file_num_timesteps.append(len(timesteps))
                 per_file_t_indexes.append(orig_t_index)
-                # normalization stats
-                self.dataset_stats[file_idx]["mean"] = f["metadata/k_mean"][:]
-                self.dataset_stats[file_idx]["std"] = f["metadata/k_std"][:]
-                self.dataset_stats[file_idx]["min"] = f["metadata/k_min"][:]
-                self.dataset_stats[file_idx]["max"] = f["metadata/k_max"][:]
-                if stats is None and normalization_scope == "dataset":
-                    stats = RunningMeanStd(shape=self.dataset_stats[file_idx]["mean"].shape)
-                if stats is not None:
-                    stats.update(self.dataset_stats[file_idx]["mean"],
-                                 self.dataset_stats[file_idx]["std"]**2,
-                                 self.dataset_stats[file_idx]["min"],
-                                 self.dataset_stats[file_idx]["max"],
-                                 count=len(timesteps))
+                if self.dataset_stats is not None:
+                    # normalization stats
+                    self.dataset_stats[file_idx]["mean"] = f["metadata/k_mean"][:]
+                    self.dataset_stats[file_idx]["std"] = f["metadata/k_std"][:]
+                    self.dataset_stats[file_idx]["min"] = f["metadata/k_min"][:]
+                    self.dataset_stats[file_idx]["max"] = f["metadata/k_max"][:]
+                    if stats is None and normalization_scope == "dataset":
+                        stats = RunningMeanStd(shape=self.dataset_stats[file_idx]["mean"].shape)
+                    if stats is not None:
+                        stats.update(self.dataset_stats[file_idx]["mean"],
+                                     self.dataset_stats[file_idx]["std"]**2,
+                                     self.dataset_stats[file_idx]["min"],
+                                     self.dataset_stats[file_idx]["max"],
+                                     count=len(timesteps))
 
-        if normalization_scope == "dataset":
+        if normalization_scope == "dataset" and normalization_stats is None:
             self.dataset_stats["full"]["mean"] = stats.mean
             self.dataset_stats["full"]["std"] = stats.var ** (1/2)
             self.dataset_stats["full"]["min"] = stats.min

eval/plot_utils.py
@@ -88,16 +88,16 @@ def plot4x4_sided(x1, x2, title="", mark_bad=False, average=True):
         other = tuple([o for o in range(5) if o != i and o != j])
 
         if average:
-            x1_plot = x1[::2].sum(0).mean(other)
-            x2_plot = x2[::2].sum(0).mean(other)
+            x1_plot = x1[0].mean(other)
+            x2_plot = x2[0].mean(other)
         else:
             x1_plot = (
-                torch.tensor(x1[::2].sum(0))
+                torch.tensor(x1[0])
                 .permute(i, j, *other)
                 .numpy()[:, :, 0, 0, 0]
             )
             x2_plot = (
-                torch.tensor(x2[::2].sum(0))
+                torch.tensor(x2[0])
                 .permute(i, j, *other)
                 .numpy()[:, :, 0, 0, 0]
             )
@@ -362,7 +362,14 @@ def generate_val_plots(x_rollout, y, ts, phase):
     }
     for name, plot_fn in val_plots_dict.items():
         # first timestep and batch
+        if y.shape[1] != 2:
+            y = torch.cat([y[:, 0::2].sum(axis=1, keepdims=True), y[:, 1::2].sum(axis=1, keepdims=True)], dim=1).cpu()
         y_first = (y[0] if y.ndim == 7 else y[0, 0]).to("cpu")
+        if x_rollout.shape[2] != 2:
+            x_rollout = torch.cat(
+                [x_rollout[:, :, 0::2].sum(axis=2, keepdims=True), x_rollout[:, :, 1::2].sum(axis=2, keepdims=True)],
+                dim=2
+            )
         plots[name] = plot_fn(x_rollout[0, 0], x2=y_first)
     return plots
 

models/swin_unet.py
@@ -517,7 +517,6 @@ class SwinUnet(nn.Module):
             patch_skip=self.patch_skip,
             # cond_dim=self.cond_embed.cond_dim,
         )
-
         self.reset_parameters()
 
     def reset_parameters(self):
@@ -532,6 +531,7 @@ class SwinUnet(nn.Module):
             up_blk.reset_parameters(self.init_weights)
             down_blk.reset_parameters(self.init_weights)
         self.middle.reset_parameters(self.init_weights)
+        self.middle_upscale.reset_parameters(self.init_weights)
 
     def forward(self, x: torch.Tensor, **kwargs) -> torch.Tensor:
         # compress to patch space