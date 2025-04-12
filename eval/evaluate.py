f2406ea3380e9f3a891071c99c3f909f53f9b743
galletti <ggalletti@tutanota.com>
2025-04-12 12:52:02 +0200

    cleanup

--- eval/evaluate.py
+++ eval/evaluate.py
@@ -12,18 +12,17 @@ from eval import (
     validation_metrics,
     generate_val_plots,
     get_rollout_fn,
+    get_flux_plot
 )
 
-
-def evaluate(
-    rank, world_size, model, loss_wrap, valsets, valloaders, opt, epoch, cfg, device, loss_val_min
-):
+def evaluate(rank, world_size, model, loss_wrap, valsets, valloaders, opt, epoch, cfg, device, gkw_args, loss_val_min):
     # Validation loop
     n_eval_steps = cfg.validation.n_eval_steps
     bundle_seq_length = cfg.model.bundle_seq_length
     tot_eval_steps = n_eval_steps * bundle_seq_length
     predict_delta = cfg.training.predict_delta
     denormalize = cfg.validation.denormalize
+    use_gkw = cfg.validation.use_gkw
     use_tqdm = cfg.logging.tqdm
     use_amp = cfg.use_amp
     input_fields = cfg.dataset.input_fields
@@ -31,6 +30,7 @@ def evaluate(
     conditioning = cfg.model.conditioning
     idx_keys = ["file_index", "timestep_index"]
     log_metric_dict = {}
+    gkw_kfiles = []
     val_plots = {}
     val_freq = cfg.validation.validate_every_n_epochs
 
@@ -48,8 +48,10 @@ def evaluate(
                 use_amp=use_amp,
             )
 
-            valname = "holdout_trajectories" if val_idx == 0 else "holdout_samples"
-            # TODO loss_wrap in metrics (flux integrals)
+            valname = (
+                "holdout_trajectories" if val_idx == 0 else "holdout_samples"
+            )
+            # TODO add loss wrapper for integrals in eval
             metric_fn_list = {
                 # NOTE: average across all dimensions except timesteps
                 "relative_norm_mse": partial(relative_norm_mse, dim_to_keep=0),
@@ -57,9 +59,7 @@ def evaluate(
             metrics = defaultdict(dict)
             for phase in ["linear", "saturated"]:
                 for key in cfg.model.losses:
-                    metrics[phase][key] = torch.zeros(
-                        [len(metric_fn_list), tot_eval_steps]
-                    )
+                    metrics[phase][key] = torch.zeros([len(metric_fn_list), tot_eval_steps])
 
             n_timesteps_acc = {
                 "linear": torch.zeros([tot_eval_steps]),
@@ -77,30 +77,17 @@ def evaluate(
             with torch.no_grad():
                 for idx, sample in enumerate(valloader):
                     sample: CycloneSample
-                    inputs = {
-                        k: getattr(sample, k).to(device, non_blocking=True)
-                        for k in input_fields
-                    }
-                    gts = {
-                        k: getattr(sample, f"y_{k}").to(device, non_blocking=True)
-                        for k in outputs
-                    }
-                    conds = {
-                        k: getattr(sample, k).to(device, non_blocking=True)
-                        for k in conditioning
-                    }
+                    inputs = {k: getattr(sample, k).to(device, non_blocking=True) for k in input_fields}
+                    gts = {k: getattr(sample, f"y_{k}").to(device, non_blocking=True) for k in outputs}
+                    conds = {k: getattr(sample, k).to(device, non_blocking=True) for k in conditioning}
                     idx_data = {k: getattr(sample, k).to(device) for k in idx_keys}
 
                     # TODO: dont hardcode this
                     phase_change = 24 if cfg.dataset.offset == 0 else 0
-                    inputs_list, gts_list, conds_list, idx_data_list, phase_list = (
-                        split_batch_into_phases(
-                            phase_change,
-                            inputs,
-                            gts,
-                            conds,
-                            idx_data,
-                        )
+                    inputs_list, gts_list, conds_list, idx_data_list, phase_list = split_batch_into_phases(
+                        phase_change,
+                        inputs, gts, conds,
+                        idx_data,
                     )
 
                     # Iterate over the splits
@@ -112,7 +99,12 @@ def evaluate(
                         phase = phase_list[i]
 
                         # get the rolled out validation trajectories
-                        rollout = rollout_fn(model, inputs, idx_data, conds)
+                        rollout = rollout_fn(
+                            model,
+                            inputs,
+                            idx_data,
+                            conds
+                        )
 
                         # denormalize rollout and target for evaluation / plots
                         if denormalize:
@@ -123,11 +115,11 @@ def evaluate(
                                         torch.stack(
                                             [
                                                 valset.denormalize(
-                                                    f, *{key: rollout[key][t, b]}
+                                                    rollout[key][t, b], f
                                                 )
                                                 for b, f in enumerate(
-                                                    idx_data["file_index"].tolist()
-                                                )
+                                                idx_data["file_index"].tolist()
+                                            )
                                             ]
                                         )
                                         for t in range(rollout[key].shape[0])
@@ -135,10 +127,8 @@ def evaluate(
                                 )
                                 gts[key] = torch.stack(
                                     [
-                                        valset.denormalize(f, *{key: gts[key][b]})
-                                        for b, f in enumerate(
-                                            idx_data["file_index"].tolist()
-                                        )
+                                        valset.denormalize(gts[key][b], f)
+                                        for b, f in enumerate(idx_data["file_index"].tolist())
                                     ]
                                 )
 
@@ -151,6 +141,32 @@ def evaluate(
                             metrics_fns=metric_fn_list,
                             get_normalized=not denormalize,
                         )
+
+                        if use_gkw:
+                            # onestep predictions (time dimension = 0)
+                            rollout_dict = {
+                                int(t_idx.item()): rollout["df"][0, b]
+                                .cpu()
+                                .numpy()
+                                for b, t_idx in enumerate(idx_data["timestep_index"])
+                            }
+                            # TODO put original file in config?
+                            src_config_path = (
+                                valset.files[0]
+                                .split("/")[-1]
+                                .replace("_ifft", "")
+                                .replace("_separate_zf", "")
+                                .replace(".h5", "")
+                            )
+                            src_config_path = (
+                                f"/restricteddata/ukaea/"
+                                f"gyrokinetics/raw/{src_config_path}"
+                            )
+                            gkw_kfiles.extend(
+                                dump_rollout(
+                                    rollout_dict, gkw_args["dump_path"], src_config_path
+                                )
+                            )
 
                         for key in metrics_i.keys():
                             if metrics_i[key].shape[-1] < tot_eval_steps:
@@ -186,7 +202,8 @@ def evaluate(
                                     phase=(
                                         "Saturated Phase"
                                         if idx == 20 or cfg.dataset.offset > 0
-                                        else "Linear Phase"
+                                        else
+                                        "Linear Phase"
                                     ),
                                 )
                                 val_plots.update(plots)
@@ -205,24 +222,15 @@ def evaluate(
                 for phase in metrics.keys():
                     cur_ts = n_timesteps_acc[phase].to(device)
                     gathered_ts = [
-                        torch.zeros_like(
-                            cur_ts, dtype=cur_ts.dtype, device=cur_ts.device
-                        )
-                        for _ in range(world_size)
-                    ]
+                        torch.zeros_like(cur_ts, dtype=cur_ts.dtype, device=cur_ts.device)
+                        for _ in range(world_size)]
                     dist.all_gather(gathered_ts, cur_ts)
                     n_timesteps_acc[phase] = torch.cat(gathered_ts).sum(0).cpu()
 
                     for m in metrics[phase].keys():
                         cur_metric = metrics[phase][m].to(device)
-                        gathered_ms = [
-                            torch.zeros_like(
-                                cur_metric,
-                                dtype=cur_metric.dtype,
-                                device=cur_metric.device,
-                            )
-                            for _ in range(world_size)
-                        ]
+                        gathered_ms = [torch.zeros_like(cur_metric, dtype=cur_metric.dtype, device=cur_metric.device)
+                                    for _ in range(world_size)]
                         dist.all_gather(gathered_ms, cur_metric)
                         gathered_ms = torch.cat(gathered_ms)
                         if len(gathered_ms.shape) == 2:
@@ -239,9 +247,7 @@ def evaluate(
 
             for phase in metrics.keys():
                 for out_type in metrics[phase].keys():
-                    metrics[phase][out_type] = metrics[phase][
-                        out_type
-                    ] / n_timesteps_acc[phase].unsqueeze(0)
+                    metrics[phase][out_type] = metrics[phase][out_type] / n_timesteps_acc[phase].unsqueeze(0)
 
             for idx, metric_name in enumerate(metric_fn_list):
                 for phase in metrics.keys():
@@ -255,6 +261,13 @@ def evaluate(
             if val_idx == 0:
                 # trajectoy validation
                 n_timesteps_acc_model_saving = n_timesteps_acc
+
+        if use_gkw:
+            # run gkw on accumulated rollout
+            gkw_args["futures"][epoch] = gkw_args["executor"].submit(
+                request_gkw_sim, gkw_args["dump_path"],
+                gkw_kfiles, cfg.logging.run_id
+            )
 
         if cfg.ckpt_path is not None and not rank:
             mse_sat = log_metric_dict[
@@ -281,6 +294,29 @@ def evaluate(
                 loss_val_min=loss_val_min,
             )
         else:
-            warnings.warn("`cfg.ckpt_path` is not set: checkpoints will not be stored")
+            warnings.warn(
+                "`cfg.ckpt_path` is not set: checkpoints will not be stored"
+            )
+
+    # check gkw future once a epoch
+    if use_gkw:
+        fluxes, potentials = {}, {}
+        done_futures = []
+        for future_epoch, fut in gkw_args["gkw_futures"].items():
+            if fut.done():
+                done_futures.append(future_epoch)
+                fluxes[future_epoch], potentials[future_epoch] = fut.result()
+                flux_plot, flux_error = get_flux_plot(
+                    fluxes[future_epoch], dataset=valsets[0]
+                )
+                gkw_logs = {
+                    "Flux": flux_plot,
+                    "val_holdout_trajectories/flux_mse": flux_error,
+                }
+                log_metric_dict = log_metric_dict | gkw_logs
+
+        # close finished futures
+        for k in done_futures:
+            del gkw_args["gkw_futures"][k]
 
     return log_metric_dict, val_plots, loss_val_min
